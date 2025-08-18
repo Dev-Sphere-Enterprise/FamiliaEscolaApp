15f85abddeabcde27eb62ef8260a15c7dce7e79a
Rian Wilker Santos Melo <78122002+RWilker87@users.noreply.github.com>
2025-08-18 14:05:08 -0300

    Modificação no caminho

--- lib/pages/add_student_page.dart
+++ lib/pages/add_student_page.dart
@@ -1,4 +1,4 @@
-import 'package:FamiliaEscolaApp/pages/student_service.dart';
+import 'package:FamiliaEscolaApp/services/student_service.dart';
 import 'package:flutter/material.dart';
 
 class AddStudentPage extends StatefulWidget {
