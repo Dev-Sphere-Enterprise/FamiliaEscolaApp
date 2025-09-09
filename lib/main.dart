d2371b9f9e4c8c62d4f2794ef13e02ebbfc192d3
Rian Wilker Santos Melo <78122002+RWilker87@users.noreply.github.com>
2025-09-09 13:39:25 -0300

    Alteração no main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'firebase_options.dart';
-import 'auth_gate.dart';              // <- use este
-import 'pages/splash_page.dart';     // <- se quiser abrir com splash
+import 'auth_gate.dart';
+import 'pages/splash_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('pt_BR', null);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -24,7 +27,6 @@ class FamiliaEscolaApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E88E5)),
         useMaterial3: true,
       ),
-      // Se quiser splash primeiro, use SplashPage(); senão, vá direto pro AuthGate();
       home: const SplashPage(),
       // home: const AuthGate(),
     );
